69dcdeaf9ab5cd158aec7fff870bca6095fecf33
Dan Nagaoka <dan.nagaoka.0@gmail.com>
2024-08-11 17:51:54 -0300

    feat: add form validation

--- src/components/SecretForm.vue
+++ src/components/SecretForm.vue
@@ -1,35 +1,54 @@
 <template>
   <v-container>
-    <v-form @submit.prevent="submitForm">
-      <v-textarea v-model="secret" label="Secret" required />
+    <v-form
+      v-model="formIsValid"
+      @submit.prevent="submitForm"
+    >
+      <v-textarea
+        v-model="secret"
+        label="Secret"
+        :rules="[rules.required]"
+      />
       <v-text-field
         v-model="expireAfterViews"
         label="Expire After Views"
-        min="1"
-        required
+        :rules="[rules.required, rules.positiveNumber]"
         type="number"
       />
       <v-text-field
         v-model="expireAfter"
         label="Expire After (minutes)"
-        min="0"
-        required
+        :rules="[rules.required, rules.positiveNumber]"
         type="number"
       />
-      <v-btn color="primary" type="submit">Submit</v-btn>
-      <v-alert v-if="responseMessage" type="success">{{ responseMessage }}</v-alert>
+      <v-btn
+        color="primary"
+        :disabled="!formIsValid"
+        type="submit"
+      > Submit </v-btn>
+      <v-alert v-if="secretHash" type="success">
+        Secret created! Hash: {{ secretHash }}
+        <br>
+        You can view it <router-link :to="`/secret/${secretHash}`">here</router-link>.
+      </v-alert>
     </v-form>
   </v-container>
 </template>
 
-<script setup>
+<script setup lang="ts">
   import { ref } from 'vue';
   import apiService from '@/services/api-service';
 
-  const secret = ref('');
-  const expireAfterViews = ref(1);
-  const expireAfter = ref(0);
-  const responseMessage = ref('');
+  const formIsValid = ref<boolean>(false);
+  const secret = ref<string>('');
+  const expireAfterViews = ref<number>(3);
+  const expireAfter = ref<number>(5);
+  const secretHash = ref<string>('');
+
+  const rules = {
+    required: (v: string) => !!v || 'This field is required',
+    positiveNumber: (v: number) => v > 0 || 'Must be greater than 0',
+  };
 
   const submitForm = async () => {
     try {
@@ -38,7 +57,7 @@
         expireAfterViews: +expireAfterViews.value,
         expireAfter: +expireAfter.value,
       });
-      responseMessage.value = `Secret created! Hash: ${response.data.hash}`;
+      secretHash.value = response.data.hash;
     } catch (error) {
       console.error('Error submitting form:', error);
     }
