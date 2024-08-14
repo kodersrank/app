a70662f27b7c3024f70de7dfc9e9984a2dfcbd58
Dan Nagaoka <dan.nagaoka.0@gmail.com>
2024-08-14 03:18:03 -0300

    feat: add flashier style to Home component

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,10 +1,10 @@
 <template>
   <v-container class="fill-height d-flex align-center justify-center">
     <div class="content d-flex justify-center">
-
-      <section class="flex-grow-1 d-flex ga-2 flex-column">
+      <section>
         <v-text-field
           v-model="hashInput"
+          class=""
           hide-details="auto"
           label="View Secret"
           placeholder="Enter hash"
@@ -14,21 +14,15 @@
           class="align-center justify-center"
           :to="`/secret/${hashInput}`"
         >
-          <v-btn color="primary">
-            View Secret
-          </v-btn>
+          <v-btn color="primary"> View Secret </v-btn>
         </router-link>
       </section>
       <div class="pa-6 d-flex flex-column justify-center">
-        <strong class="text-uppercase">
-          or
-        </strong>
+        <strong class="text-uppercase"> or </strong>
       </div>
-      <section class="flex-grow-1 d-flex align-center justify-start">
+      <section>
         <router-link class="d-flex justify-start" to="/secret">
-          <v-btn color="secondary">
-            Create a New Secret
-          </v-btn>
+          <v-btn color="secondary"> Create a New Secret </v-btn>
         </router-link>
       </section>
     </div>
@@ -44,12 +38,50 @@
 <style scoped lang="scss">
 .content {
   width: fit-content;
+  height: 100%;
 
   > section {
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    transition: outline .6s;
+    outline: 3px solid transparent;
+    border-radius: 8px;
+    height: 100%;
     min-width: 400px;
+    padding: 10px;
+
+    --boost: 0%;
+
+    background-image: radial-gradient(
+        circle at var(--side),
+        var(--color) calc(-100% + var(--boost)),
+        rgba(0, 0, 0, 0.4) 100%
+      ),
+      linear-gradient(135deg, rgba(255, 255, 255, 0.1), rgba(0, 0, 0, 0.2) 70%),
+      radial-gradient(circle, rgba(0, 0, 0, 0.5), transparent);
+
+    &:hover {
+      --boost: 100%;
+      outline: 4px solid var(--color);
+    }
+
+    .v-input {
+      max-height: 50px;
+    }
 
     .v-btn {
       width: 400px;
+    }
+
+    &:first-child {
+      --side: right;
+      --color: rgb(var(--v-theme-primary));
+    }
+
+    &:last-child {
+      --side: left;
+      --color: rgb(var(--v-theme-secondary));
     }
   }
 }
