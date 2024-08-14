0bf951fdc5f8d77daf5a8f455ac3c8ffec626207
Dan Nagaoka <dan.nagaoka.0@gmail.com>
2024-08-14 14:54:21 -0300

    feat: add main-bg

--- src/layouts/default.vue
+++ src/layouts/default.vue
@@ -1,4 +1,5 @@
 <template>
+  <img class="bg-underlay" src="@/assets/main-bg.jpg">
   <v-app>
     <v-main>
       <router-view />
@@ -9,3 +10,17 @@
 <script lang="ts" setup>
   //
 </script>
+
+<style scoped lang="scss">
+.bg-underlay {
+  position: fixed;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  height: 100vh;
+  width: 100vw;
+  object-fit: cover;
+  filter: blur(1px) brightness(50%);
+  z-index: 0;
+}
+</style>
