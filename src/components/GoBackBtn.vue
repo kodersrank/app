ae369af5b12d9e8a463f10f2d847e362abb42f53
Dan Nagaoka <dan.nagaoka.0@gmail.com>
2024-08-14 05:20:27 -0300

    feat: add GoBackBtn

---
+++ src/components/GoBackBtn.vue
@@ -0,0 +1,25 @@
+<template>
+  <v-btn class="go-back-btn" variant="tonal" @click="goHome">
+    ⬅ Go Back
+  </v-btn>
+</template>
+
+<script setup lang="ts">
+  import { useRouter } from 'vue-router';
+
+  const router = useRouter();
+
+  const goHome = () => {
+    router.push('/');
+  };
+</script>
+
+<style scoped lang="scss">
+.go-back-btn {
+  position: fixed;
+  top: 1rem;
+  left: 1rem;
+  color: white;
+  z-index: 1000;
+}
+</style>
